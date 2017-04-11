7b10c5b8fd0c70d05bb0049af733680195742615
Wreulicke <masaya2dies@gmail.com>
2017-04-11 21:43:15 +0900

    add jackson sandbox

---
+++ src/test/java/com/github/wreulicke/jackson/JacksonTest.java
@@ -0,0 +1,42 @@
+package com.github.wreulicke.jackson;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Data;
+import lombok.Value;
+
+public class JacksonTest {
+  @Test
+  public void test1() throws JsonParseException, JsonMappingException, IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    Hoge hoge = mapper.readValue("{ \"str\" : \"test\" }", Hoge.class);
+    System.out.println(hoge);
+    Fuga fuga = mapper.readValue("{ \"str\" : \"test\" }", Fuga.class);
+    System.out.println(fuga);
+    Moga moga = mapper.readValue("{ \"str\" : \"test\" }", Moga.class);
+    System.out.println(moga);
+
+  }
+
+  @Data
+  public static class Fuga {
+    private String str;
+  }
+
+  @Value
+  public static class Hoge {
+    private String str;
+  }
+
+  @Value
+  public static class Moga {
+    private String str;
+    private String yyy;
+  }
+}
