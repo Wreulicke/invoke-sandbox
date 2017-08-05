fec4e32e61c57fbd64b13e295b1ad9693e630836
wreulicke <saito.masaya@classmethod.jp>
2017-08-06 01:36:09 +0900

    refactor

--- src/test/java/com/github/wreulicke/reflect/ClassTest.java
+++ src/test/java/com/github/wreulicke/reflect/ClassTest.java
@@ -11,6 +11,7 @@ import java.lang.reflect.Proxy;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ClassTest {
@@ -20,89 +21,171 @@ class ClassTest {
     }
   }
 
-  /**
-   * case for class
-   */
-  @Test
-  void test1() {
-    assertThat(ClassTest.class.getName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
-    assertThat(new ClassTest().getClass()
-      .getName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
+  @Nested
+  class 基本的なクラスのテスト {
 
-    assertThat(EnclosedType.class.getName()).isEqualTo("com.github.wreulicke.reflect.ClassTest$EnclosedType");
-    assertThat(EnclosedType.class.getEnclosingClass()).isEqualTo(ClassTest.class);
+    @Test
+    void testGetName() {
+      assertThat(ClassTest.class.getName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
+    }
 
-    assertThat(ClassTest.class.getCanonicalName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
-    assertThat(EnclosedType.class.getCanonicalName()).isEqualTo("com.github.wreulicke.reflect.ClassTest.EnclosedType");
-    assertThat(ClassTest.class.getSimpleName()).isEqualTo("ClassTest");
-    assertThat(EnclosedType.class.getSimpleName()).isEqualTo("EnclosedType");
+    @Test
+    void testGetCanonicalName() {
+      assertThat(ClassTest.class.getCanonicalName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
+    }
 
-    assertThat(ClassTest.class.getTypeName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
-    assertThat(ClassTest.class.getTypeParameters()).isEmpty();
+    @Test
+    void testGetSimpleName() {
+      assertThat(ClassTest.class.getSimpleName()).isEqualTo("ClassTest");
+    }
+
+    @Test
+    void testGetTypeName() {
+      assertThat(ClassTest.class.getTypeName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
+    }
+
+    @Test
+    void testGetTypeParameter() {
+      assertThat(ClassTest.class.getTypeParameters()).isEmpty();
+    }
+
+    @Test
+    void testGetClassInstance() {
+      ClassTest test = new ClassTest();
+      assertThat(test.getClass()).isEqualTo(ClassTest.class);
+      assertThat(test.getClass()
+        .getName()).isEqualTo("com.github.wreulicke.reflect.ClassTest");
+    }
+
+    @Test
+    void testAssignable() {
+      assertThat(ClassTest.class.isAssignableFrom(new ClassTest() {}.getClass())).isTrue();
+    }
+
+    @Test
+    void testAssignableLocalClass() {
+      class LocalClass extends ClassTest {
+      }
+      assertThat(ClassTest.class.isAssignableFrom(LocalClass.class)).isTrue();
+    }
+
+
+    @Test
+    void testAssignableOther() {
+      assertThat(ClassTest.class.isAssignableFrom(new Object() {}.getClass())).isFalse();
+    }
+
   }
 
-  @SafeVarargs
-  public static <T> Class<?> clazzes(T... arrays) {
-    return arrays.getClass()
-      .getComponentType();
+  class Enclosedなクラス {
+
+    @Test
+    void testGetName() {
+      assertThat(EnclosedType.class.getName()).isEqualTo("com.github.wreulicke.reflect.ClassTest$EnclosedType");
+    }
+
+    @Test
+    void testGetSimpleName() {
+      assertThat(EnclosedType.class.getSimpleName()).isEqualTo("EnclosedType");
+    }
+
+    @Test
+    void testGetEnclosingClass() {
+      assertThat(EnclosedType.class.getEnclosingClass()).isEqualTo(ClassTest.class);
+    }
+
+    @Test
+    void testGetCanonicalName() {
+      assertThat(EnclosedType.class.getCanonicalName()).isEqualTo("com.github.wreulicke.reflect.ClassTest.EnclosedType");
+    }
+
   }
 
-  /**
-   * case for array and class
-   */
-  @Test
-  void test2() {
+
+  @Nested
+  class 配列の周辺 {
     Class<?> clazz1 = ClassTest.class;
     Class<?> clazz2 = ClassTest[].class;
 
-    assertThat(clazz2.getComponentType()).isEqualTo(clazz1);
-    assertThat(clazz1.isArray()).isFalse();
-    assertThat(clazz2.isArray()).isTrue();
-    assertThat(clazzes(new ClassTest())).isEqualTo(ClassTest.class);
+    @Test
+    void testGetComponentType() {
+      assertThat(clazz2.getComponentType()).isEqualTo(clazz1);
+    }
+
+    @Test
+    void testIsArray1() {
+      assertThat(clazz1.isArray()).isFalse();
+    }
+
+    @Test
+    void testIsArray2() {
+      assertThat(clazz2.isArray()).isTrue();
+    }
+
+    public <T> Class<?> getComponentType(@SuppressWarnings("unchecked") T... arrays) {
+      return arrays.getClass()
+        .getComponentType();
+    }
+
+    @Test
+    void testVariadicMethod() {
+      assertThat(getComponentType(new ClassTest())).isEqualTo(ClassTest.class);
+    }
   }
 
+  @Nested
+  class 合成って何 {
+    @Test
+    void testSimple() {
+      assertThat(ClassTest.class.isSynthetic()).isFalse();
+    }
 
-  /**
-   * sythesized
-   * 
-   * @throws IllegalAccessException
-   * @throws NoSuchFieldException
-   */
-  @Test
-  void test3() throws NoSuchFieldException, IllegalAccessException {
-    assertThat(ClassTest.class.isSynthetic()).isFalse();
-    assertThat(EnclosedType.class.isSynthetic()).isFalse();
-    assertThat(new Object() {}.getClass()
-      .isSynthetic()).isFalse();
-    assertThat(new EnclosedType().new Inner().getClass()
-      .isSynthetic()).isFalse();
-    assertThat(ClassTest[].class.isSynthetic()).isFalse();
+    @Test
+    void testEnclosed() {
+      assertThat(EnclosedType.class.isSynthetic()).isFalse();
+    }
 
-    // Proxy class is synthesized
-    assertThat(Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[] {
-      Predicate.class
-    }, (proxy, name, args) -> null)
-      .getClass()
-      .isSynthetic()).isFalse();
+    @Test
+    void testAnonymousClass() {
+      assertThat(new Object() {}.getClass()
+        .isSynthetic()).isFalse();
+    }
 
-    // lambda expression instance is synthesized
-    assertThat(((Predicate<Integer>) (Integer i) -> true).getClass()
-      .isSynthetic()).isTrue();
+    @Test
+    void testInnerClass() {
+      assertThat(EnclosedType.Inner.class.isSynthetic()).isFalse();
+      assertThat(new EnclosedType().new Inner().getClass()
+        .isSynthetic()).isFalse();
+    }
 
-    // method handle proxies is not synthesized
-    Lookup lookup = MethodHandles.lookup();
-    MethodHandle handle = lookup.findStaticGetter(System.class, "out", PrintStream.class);
-    assertThat(MethodHandleProxies.asInterfaceInstance(Supplier.class, handle)
-      .getClass()
-      .isSynthetic()).isFalse();
-  }
+    @Test
+    void testArrayType() {
+      assertThat(ClassTest[].class.isSynthetic()).isFalse();
+    }
 
-  @Test
-  void test4() {
-    ClassTest.class.cast(new ClassTest());
-    ClassTest.class.isAssignableFrom(new ClassTest().getClass());
-    new ClassTest() {}.getClass()
-      .asSubclass(ClassTest.class);
+    @Test
+    void testProxy() {
+      assertThat(Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[] {
+        Predicate.class
+      }, (proxy, name, args) -> null)
+        .getClass()
+        .isSynthetic()).isFalse();
+    }
+
+    @Test
+    void testLambda() {
+      assertThat(((Predicate<Integer>) (Integer i) -> true).getClass()
+        .isSynthetic()).isTrue();
+    }
+
+    @Test
+    void testMethodHandle() throws NoSuchFieldException, IllegalAccessException {
+      Lookup lookup = MethodHandles.publicLookup();
+      MethodHandle handle = lookup.findStaticGetter(System.class, "out", PrintStream.class);
+      assertThat(MethodHandleProxies.asInterfaceInstance(Supplier.class, handle)
+        .getClass()
+        .isSynthetic()).isFalse();
+    }
   }
 
 
